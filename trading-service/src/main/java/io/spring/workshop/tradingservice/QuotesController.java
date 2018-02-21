125e549ea56daafa553b49dc078fd7cea1e5cdc3
Stephane Maldini <smaldini@pivotal.io>
2018-02-20 20:44:48 -0800

    nicer rest template

--- trading-service/src/main/java/io/spring/workshop/tradingservice/QuotesController.java
+++ trading-service/src/main/java/io/spring/workshop/tradingservice/QuotesController.java
@@ -1,6 +1,9 @@
 package io.spring.workshop.tradingservice;
 
 import java.net.URI;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -10,8 +13,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriTemplate;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -53,45 +61,50 @@ public class QuotesController {
 
 	@GetMapping(path = "/quotes/summary/{ticker}", produces = APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public TradingCompanySummary quotesDetails(@PathVariable String ticker) throws Exception {
-		RequestEntity requestEntity = RequestEntity.get(new URI("http://localhost:8081/quotes?take=7"))
-				.accept(APPLICATION_JSON)
-				.build();
+	public TradingCompanySummary quotesDetails(@PathVariable String ticker) {
+		TradingCompany tradingCompany = this.restTemplate.exchange(
+				get("http://localhost:8082/details/{ticker}", ticker),
+				TradingCompany.class)
+		        .getBody();
 
-
-		TradingCompany company = this.tradingCompanyService.getTradingCompany(ticker);
-
-		if (company == null) {
-			throw new TickerNotFoundException("Unknown ticker: "+ticker);
+		if (tradingCompany == null) {
+			throw new TickerNotFoundException(String.format("Unknown ticker: %s", ticker));
 		}
 
-		AtomicBoolean timeout = new AtomicBoolean(false);
 
-		ScheduledFuture<?> f = executorService.schedule(() -> timeout.set(true), 15, TimeUnit.SECONDS);
+		Quote quote = null;
+		Instant timeout = Instant.now().plus(Duration.ofSeconds(15));
 
-		Quote match = null;
-		while (match == null && !timeout.get()) {
+		while (quote == null && Instant.now().isBefore(timeout)) {
+			List<Quote> candidates = this.restTemplate.exchange(
+					get("http://localhost:8081/quotes?take=7"),
+					new ParameterizedTypeReference<List<Quote>>() {
+					})
+			        .getBody();
 
-			List<Quote> latest = this.restTemplate.exchange(requestEntity, new ParameterizedTypeReference<List<Quote>>() {}).getBody();
-
-			match = latest.stream()
-					.filter(q -> q.getTicker().equalsIgnoreCase(ticker))
-					.findAny()
-					.orElse(null);
-		}
-		f.cancel(true);
-
-		if (match == null) {
-			match = new Quote(ticker);
+			quote = candidates.stream()
+			                  .filter(q -> q.getTicker().equalsIgnoreCase(ticker))
+			                  .findAny()
+			                  .orElse(null);
 		}
 
-		return new TradingCompanySummary(match, company);
+		if (quote == null) {
+			quote = new Quote(ticker);
+		}
 
+		return new TradingCompanySummary(quote, tradingCompany);
 	}
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(TickerNotFoundException.class)
 	public void onTickerNotFound() {
+	}
+
+	private RequestEntity<?> get(String uri, Object... args) {
+		return RequestEntity
+				.get(new UriTemplate(uri).expand(args))
+				.accept(APPLICATION_JSON)
+				.build();
 	}
 
 }
